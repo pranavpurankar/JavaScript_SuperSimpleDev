4a3a5515acf6edbf8f9d341a3fc3f2111c6dff67
pranavpurankar <pranav.purankar@gmail.com>
2024-08-23 09:41:08 +0530

    Scripts folder and dom-project is updated

--- Tutorial/scripts/11-todo-list.js
+++ Tutorial/scripts/11-todo-list.js
@@ -1,5 +1,7 @@
 /*
-Steps (Algorithm)
+
+// Stopped at Arrays and loops
+Algorithm:
 1. Create array to store todos
 2. When we click "Add"
 3. Get text from textbox
@@ -10,9 +12,5 @@ Steps (Algorithm)
 const todoList = [];
 
 function addTodo(){
-    const inputElement = document.querySelector('.js-name-input');
-    const name = inputElement.value;
-    todoList.push(name);
-    console.log(todoList);
-    inputElement.value='';
+    document.querySelector('.js-name-input');
 }
